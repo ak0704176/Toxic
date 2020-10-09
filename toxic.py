7e85e11d9d9a3bfea6f6f98e0ddb72eb051a4f97
Ashutosh Kumar <70029422+ak0704176@users.noreply.github.com>
2020-10-09 09:44:52 +0530

    Main Shell For Running Tokens
    BackBone Of Programming Language.

---
+++ toxic.py
@@ -0,0 +1,14 @@
+import tokens
+
+while True:
+	text = input('toxic > ')
+	if text.strip() == "": continue
+	result, error = tokens.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
